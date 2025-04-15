fdf30bb9b965dd5c5ad2047315c219cde553aa1f
Pradip <pradip@Pradips-MacBook-Air.local>
2025-04-15 23:40:48 +0530

    Promise example.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,69 +1,20 @@
-import { useState } from 'react';
+const myPromise = new Promise((resolve, reject) => {
+  // Do something async, like setTimeout
+  setTimeout(() => {
+    const success = true;
 
-export default function Form() {
-  const [answer, setAnswer] = useState('');
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('typing');
-
-  if (status === 'success') {
-    return <h1>That's right!</h1>
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus('submitting');
-    try {
-      await submitForm(answer);
-      setStatus('success');
-    } catch (err) {
-      setStatus('typing');
-      setError(err);
+    if (success) {
+      resolve("✅ It worked!");
+    } else {
+      reject("❌ Something went wrong.");
     }
-  }
+  }, 1000);
+});
 
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
-  }
-
-  return (
-    <>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === 'submitting'}
-        />
-        <br />
-        <button disabled={
-          answer.length === 0 ||
-          status === 'submitting'
-        }>
-          Submit
-        </button>
-        {error !== null &&
-          <p className="Error">
-            {error.message}
-          </p>
-        }
-      </form>
-    </>
-  );
-}
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== 'lima'
-      if (shouldError) {
-        reject(new Error('Good guess but a wrong answer. Try again!'));
-      } else {
-        resolve();
-      }
-    }, 1000);
+myPromise
+  .then(result => {
+    console.log(result); // If resolved
+  })
+  .catch(error => {
+    console.error(error); // If rejected
   });
-}
