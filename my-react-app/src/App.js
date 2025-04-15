a81abab0abe20debf0a515ed62bd5f85993ea8dc
Pradip <pradip@Pradips-MacBook-Air.local>
2025-04-15 09:38:05 +0530

    Updating object in state.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,94 @@
-import React from 'react';
 import { useState } from 'react';
 
-export default function App(){
-  const [count, setCount] = useState(0);
+export default function Form() {
+  const [person, setPerson] = useState({
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
+  });
 
-  function handleClick(num) {
-    setCount(count + num);
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value
+      }
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value
+      }
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value
+      }
+    });
   }
 
   return (
-    <div>
-      <h1>My React App</h1>
-      <button onClick={()=>handleClick(1)}>+1</button>
-      <button onClick={()=>handleClick(3)}>+3</button>
-
-
-      <p>Count: {count}</p>
-
-
-    </div>
+    <>
+      <label>
+        Name:
+        <input
+          value={person.name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        Title:
+        <input
+          value={person.artwork.title}
+          onChange={handleTitleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          value={person.artwork.city}
+          onChange={handleCityChange}
+        />
+      </label>
+      <label>
+        Image:
+        <input
+          value={person.artwork.image}
+          onChange={handleImageChange}
+        />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i>
+        {' by '}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
+      </p>
+      <img
+        src={person.artwork.image}
+        alt={person.artwork.title}        
+        style={{ border: '2px solid red', width:'200px', height:"200px" }}
+      />
+    </>
   );
-} 
+}
