d80adb80e3c02691b7afa396f8509ae872421f4b
Pradip <pradip@Pradips-MacBook-Air.local>
2025-04-15 11:58:01 +0530

    Promise

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,27 +1,69 @@
-export default function App() {
-  function handleClick() {
-    const button = document.querySelector('#toggleButton');
-    if (button.style.backgroundColor === 'black') {
-      button.style.backgroundColor = 'white';
-      button.style.color = 'black';
-    } else {
-      button.style.backgroundColor = 'black';
-      button.style.color = 'white';
+import { useState } from 'react';
+
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
     }
   }
 
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
   return (
-    <button
-      id="toggleButton"
-      onClick={handleClick}
-      style={{
-        backgroundColor: 'blue',
-        color: 'white',
-        padding: '10px',
-        borderRadius: '5px'
-      }}
-    >
-      Toggle the lights
-    </button>
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+      </form>
+    </>
   );
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
